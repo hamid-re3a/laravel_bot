61d30ca7dcb08af79e401a1c90936e6674dc467b
MehranMirkhan <mehranmirkhan@gmail.com>
2019-10-03 13:27:33 +0330

    Instagram command

--- app/Http/Controllers/api/v1/TelegramController.php
+++ app/Http/Controllers/api/v1/TelegramController.php
@@ -7,15 +7,20 @@ use App\InstagramAccount;
 use App\TelegramUser;
 
 class TelegramController extends ApiController {
-    private static $idleState;
+    private static $states;
 
     public function dokan() {
-        TelegramController::$idleState = "";
+        TelegramController::$states = [];
+        TelegramController::$states->init = "";
+        TelegramController::$states->instaUsername = "instagram_username";
+        TelegramController::$states->instaPassword = "instagram_password";
         try {
             $update   = @file_get_contents("php://input");
             $telegram = new TelegramSdk(env('TELEGRAM_DOKAN_API_KEY'));
             $telegram->intitilize($update);
 //            $telegram->sendMessage(null, $update);
+
+            $telegram->sendMessage(null, json_encode(InstagramAccount::all()));
 
             switch ($telegram->type) {
                 case "message":
@@ -40,7 +45,7 @@ class TelegramController extends ApiController {
         $tel_user->username = $tel->username;
         $tel_user->first_name = $tel->first_name;
         $tel_user->last_name = $tel->last_name;
-        $tel_user->state = TelegramController::$idleState;
+        $tel_user->state = TelegramController::$states->init;
         $tel_user->carry = "";
         $tel_user->save();
         return $tel_user;
@@ -49,7 +54,33 @@ class TelegramController extends ApiController {
     private function handleMessage($tel) {
         $tel_user = $this->getTelegramUser($tel);
         switch ($tel_user->state) {
-            case TelegramController::$idleState:
+            case TelegramController::$states->init:
+                $this->s_idle($tel, $tel_user);
+                break;
+            case TelegramController::$states->instaUsername:
+                $tel->sendMessage(null, "[DEBUG] In state instaUsername");
+                $carry = ["username" => $tel->message];
+                $tel_user->carry = json_encode($carry);
+                $tel_user->save();
+                $tel->sendMessage(null, "[DEBUG] Carry saved");
+                $tel->sendMessage(null, "لطفاً رمز عبور اینستاگرام خود را وارد نمایید:");
+                $tel_user->state = TelegramController::$states->instaPassword;
+                $tel_user->save();
+                break;
+            case TelegramController::$states->instaPassword:
+                $tel->sendMessage(null, "[DEBUG] In state instaPassword");
+                $carry = json_decode($tel_user->carry);
+                $tel->sendMessage(null, "[DEBUG] Carry decoded");
+                $instaAccount = new InstagramAccount();
+                $instaAccount->telegram_user_id = $tel->chat_id;
+                $instaAccount->username = $carry->username;
+                $instaAccount->password = $tel->message;
+                $instaAccount->paid_until = date_default_timezone_get();
+                $instaAccount->save();
+                $tel->sendMessage(null, "[DEBUG] Insta account created");
+                $tel_user->state = TelegramController::$states->init;
+                $tel_user->save();
+                $tel->message = "افزایش فالوور اینستاگرام";
                 $this->s_idle($tel, $tel_user);
                 break;
         }
@@ -82,10 +113,12 @@ class TelegramController extends ApiController {
             case $instaBtn:
                 $instaAccounts = InstagramAccount::where("telegram_user_id", $tel_user->telegram_id)->get();
                 $tel->sendMessage(null, json_encode($instaAccounts));
-                if (count($instaAccounts) == 0)
-                    $tel->sendMessage(null, "You do not have an instagram account");
-                else
-                    $tel->sendMessage(null, "You have an instagram account");
+                if (count($instaAccounts) == 0) {
+                    $tel_user->state = TelegramController::$states->instaUsername;
+                    $tel_user->save();
+                    $tel->sendMessage(null, "لطفاً نام کاربری اینستاگرام خود را وارد نمایید:");
+                } else
+                    $tel->sendMessage(null, "You already have an instagram account");
                 break;
         }
     }
